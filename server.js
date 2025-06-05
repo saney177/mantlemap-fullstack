64f351e2eafb91df8028f50e036ecadf3dee811e
saney177 <reshetov24274@gmail.com>
2025-06-06 00:04:47 +0300

    Update server.js
    Replaced app.use('*', ...) with just app.use(...) - this is the standard way to handle all raw routes in Express.

--- server.js
+++ server.js
@@ -561,7 +561,7 @@ app.post('/api/users', async (req, res) => {
 });
 
 // --- MIDDLEWARE ДЛЯ ОБРАБОТКИ 404 ---
-app.use('*', (req, res) => {
+app.use((req, res, next) => {
     console.log(`404 - Маршрут не найден: ${req.method} ${req.originalUrl}`);
     res.status(404).json({
         success: false,
